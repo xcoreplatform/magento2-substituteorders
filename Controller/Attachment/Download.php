5d39b1134c64fd0fa4654baa7bc92ef49835cf9a
Daniel Oldersma <d.oldersma@linq5.nl>
2019-09-02 13:23:10 +0200

    check op tonen bijlage aangepast naar volgen instelling

--- Controller/Attachment/Download.php
+++ Controller/Attachment/Download.php
@@ -21,6 +21,9 @@
 
 namespace Dealer4Dealer\SubstituteOrders\Controller\Attachment;
 
+use Dealer4Dealer\SubstituteOrders\Api\Data\InvoiceInterface;
+use Dealer4Dealer\SubstituteOrders\Model\Attachment;
+use Magento\Customer\Model\Customer;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
 
@@ -32,17 +35,39 @@ class Download extends \Magento\Framework\App\Action\Action
     protected $fileFactory;
     protected $attachmentRepository;
 
+    /** @var \Magento\Customer\Api\CustomerRepositoryInterface  */
+    protected $_customerRepository;
+
+    /** @var \Magento\Framework\App\Config\ScopeConfigInterface */
+    protected $_scopeConfig;
+
+    /** @var \Dealer4Dealer\SubstituteOrders\Model\OrderFactory */
+    protected $orderFactory;
+
+    /** @var \Dealer4Dealer\SubstituteOrders\Model\InvoiceFactory */
+    protected $invoiceFactory;
+
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Customer\Model\Session $customerSession,
         \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
         \Magento\Framework\App\Response\Http\FileFactory $fileFactory,
-        \Dealer4Dealer\SubstituteOrders\Model\AttachmentRepository $attachmentRepository
+        \Dealer4Dealer\SubstituteOrders\Model\AttachmentRepository $attachmentRepository,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        \Dealer4Dealer\SubstituteOrders\Model\OrderFactory $orderFactory,
+        \Dealer4Dealer\SubstituteOrders\Model\InvoiceFactory $invoiceFactory
     ) {
         $this->resultRawFactory = $resultRawFactory;
         $this->customerSession = $customerSession;
         $this->fileFactory = $fileFactory;
         $this->attachmentRepository = $attachmentRepository;
+        $this->orderFactory = $orderFactory;
+        $this->invoiceFactory = $invoiceFactory;
+
+        $this->_scopeConfig = $scopeConfig;
+        $this->_customerRepository = $customerRepository;
+
         parent::__construct($context);
     }
 
@@ -73,9 +98,8 @@ class Download extends \Magento\Framework\App\Action\Action
 
         $attachment = $this->attachmentRepository->getById($id);
         $customer = $this->customerSession->getCustomer();
-        $magentoCustomerIdentifier = $customer->getData('entity_id');
 
-        if ($attachment->getMagentoCustomerIdentifier() != $magentoCustomerIdentifier) {
+        if (!$this->canDownloadAttachment($attachment, $customer)) {
             $this->messageManager->addError(__('Permission to download attachment denied'));
             return $this->resultRedirectFactory->create()->setPath('substitute/order');
         }
@@ -106,5 +130,58 @@ class Download extends \Magento\Framework\App\Action\Action
 
         $resultRaw = $this->resultRawFactory->create();
         return $resultRaw;
+    }
+
+    /**
+     * @param $attachment \Dealer4Dealer\SubstituteOrders\Api\Data\AttachmentInterface
+     * @param $customer Customer
+     */
+    public function canDownloadAttachment($attachment, $customer){
+         /** @var int $customerId */
+        $customerId = $customer->getid();
+        /** @var \Magento\Customer\Api\Data\CustomerInterface $mCustomer */
+        $mCustomer = $this->_customerRepository->getById($customerId);
+
+        /** @var \Magento\Framework\Api\AttributeInterface */
+        $externalCustomerIdAttribute = $mCustomer->getCustomAttribute("external_customer_id");
+        $selectOrderBySetting = $this->_scopeConfig->getValue(
+            'substitute/general/select_orders_by',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+
+        // als ik de order mag zien, dan ook de bijlage.
+        $entityId = $attachment->getEntityTypeIdentifier();
+        if ($attachment->getEntityType() === 'order') {
+            $order = $this->orderFactory->create()->load($entityId);
+        } else{
+            /** @var InvoiceInterface $invoice */
+            $invoice = $this->invoiceFactory->create()->load($entityId);
+            $orders = $invoice->getOrderIds();
+            $order = $this->orderFactory->create()->load($orders[0]);
+        }
+
+        $customerSelectionId = $customerId;
+        if ($selectOrderBySetting === 'external_customer_id' && $externalCustomerIdAttribute !== null && $externalCustomerIdAttribute->getValue() !== ''){
+            $customerSelectionId = $externalCustomerIdAttribute->getValue();
+
+            $event = new \Magento\Framework\DataObject([
+                'order' => $order,
+                'customer' => $mCustomer,
+                'hasAccess' => $order->getData($selectOrderBySetting) == $customerSelectionId,
+            ]);
+        } else {
+            $event = new \Magento\Framework\DataObject([
+                'order' => $order,
+                'customer' => $mCustomer,
+                'hasAccess' => $order->getData('magento_customer_id') == $customerSelectionId,
+            ]);
+        }
+
+        $this->_eventManager->dispatch(
+            'substituteorder_customer_has_order_access',
+            ['event' => $event]
+        );
+
+        return $event->getData('hasAccess');
     }
 }
