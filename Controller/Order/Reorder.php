3d8f615753317ca0f35d6fa2ce68754bc562e096
Willie <velislavhuizing@hotmail.com>
2023-03-17 15:00:46 +0100

    changed non-existing variable $product to $item

--- Controller/Order/Reorder.php
+++ Controller/Order/Reorder.php
@@ -140,7 +140,7 @@ class Reorder extends \Magento\Framework\App\Action\Action
                 } catch (\Magento\Framework\Exception\LocalizedException $e) {
                     $errors[] = $e->getMessage();
                 } catch (\Exception $e) {
-                    $errors[] = __("We can\'t add ({$product->getName()}) item to your shopping cart right now.");
+                    $errors[] = __("We can\'t add ({$item->getName()}) item to your shopping cart right now.");
                 }
             }
             $this->cart->save();
